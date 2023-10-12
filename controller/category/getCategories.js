88f98196f228597300393bd1610ab0742c724619
Rostyslav <107403659+28Rostyk@users.noreply.github.com>
2023-10-12 16:55:40 +0300

    update getCategories

--- controller/category/getCategories.js
+++ controller/category/getCategories.js
@@ -4,9 +4,7 @@ const { ctrlWrapper } = require("../../helpers");
 const getCategories = async (req, res) => {
   try {
     const category = await Category.find({}, "-createdAt -updatedAt");
-    res.json({
-      category: category,
-    });
+    res.json(category);
   } catch (error) {
     console.log("Error fetching categories:", error);
     res.status(500).json({ message: "Error fetching categories" });
