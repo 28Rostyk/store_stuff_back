bc115a9444b95fb277897fdf95ca5910d5664866
Rostyslav <107403659+28Rostyk@users.noreply.github.com>
2023-10-16 23:31:24 +0300

    add pagination for getproducts

--- controller/products/getProducts.js
+++ controller/products/getProducts.js
@@ -2,9 +2,35 @@ const { Products } = require("../../models");
 const { ctrlWrapper } = require("../../helpers");
 
 const getProducts = async (req, res) => {
+  const {
+    page = 1,
+    perPage = 10,
+    sortBy = "createdAt",
+    sortOrder = "asc",
+  } = req.query;
+
   try {
-    const category = await Products.find({}, "-createdAt -updatedAt");
-    res.json(category);
+    const skip = (page - 1) * perPage;
+
+    const totalCount = await Products.countDocuments();
+    const totalPages = Math.ceil(totalCount / perPage);
+
+    const sortField = sortBy === "price" ? "price" : "createdAt";
+    const sortDirection = sortOrder === "desc" ? -1 : 1;
+    const sortOptions = { [sortField]: sortDirection };
+
+    const products = await Products.find({}, "-createdAt -updatedAt")
+      .sort(sortOptions)
+      .skip(skip)
+      .limit(perPage);
+
+    res.json({
+      totalPage: totalPages,
+      totalItems: totalCount,
+      perPage: perPage,
+      currentPage: page,
+      products: products,
+    });
   } catch (error) {
     console.log("Error fetching categories:", error);
     res.status(500).json({ message: "Error fetching categories" });
